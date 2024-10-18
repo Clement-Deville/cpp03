4e4fefba8f13219103b8aa559efd3ef5ce5ca03e
Clement Deville <cdeville@made-f0Br6s15.clusters.42paris.fr>
2024-10-18 12:08:58 +0200

    ex01 done

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 17:33:56 by cdeville          #+#    #+#             */
-/*   Updated: 2024/10/17 18:21:52 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/10/18 12:04:07 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,54 @@ ScavTrap::~ScavTrap()
 	std::cout << "\e[0;31mScavTrap Destructor called\e[0m" << std::endl;
 }
 
-
 void	ScavTrap::attack(const std::string& target)
 {
-    (void)target;
+	if (this->_name.empty())
+	{
+		std::cerr << S_ATCK_INIT_MSG;
+		return ;
+	}
+	if (target.empty())
+	{
+		std::cerr << S_ATCK_TRGT_MSG;
+		return ;
+	}
+	if (this->_hit_point == 0)
+	{
+		std::cerr << S_ATCK_DEAD_MSG;
+		return ;
+	}
+	if (this->_energy_point == 0)
+	{
+		std::cerr << S_ATCK_NO_NRG_MSG;
+		return ;
+	}
+	this->_energy_point--;
+	std::cout << "ScavTrap "<< UGRN + this->_name + reset
+		<< " attacks " << UYEL + target + reset << ", causing " << BRED
+		<< this->_attack_dmg << reset
+		<< (this->_attack_dmg > 1 ? " points" : " point")
+		<< " of damage!" << std::endl;
 }
-void    ScavTrap::guardGate(void)
+
+void	ScavTrap::guardGate(void)
 {
+	if (this->_name.empty())
+	{
+		std::cerr << S_GRD_INIT_MSG;
+		return ;
+	}
+	if (this->_hit_point == 0)
+	{
+		std::cerr << S_GRD_DEAD_MSG;
+		return ;
+	}
+	if (this->_energy_point == 0)
+	{
+		std::cerr << S_GRD_NO_NRG_MSG;
+		return ;
+	}
+	this->_energy_point--;
+	std::cout << "ScavTrap "<< UGRN + this->_name + reset
+		<< " is now garding the gate, Benjamin is safe." << std::endl;
 }
--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 17:22:34 by cdeville          #+#    #+#             */
-/*   Updated: 2024/10/17 18:24:35 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/10/18 12:04:59 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,42 @@
 
 class ScavTrap : public ClapTrap
 {
-private:
 public:
-    ScavTrap(void);
-    ScavTrap(const std::string name);
-    ScavTrap(const ScavTrap &Cpy);
-    ~ScavTrap();
-    
+	ScavTrap(void);
+	ScavTrap(const std::string name);
+	ScavTrap(const ScavTrap &Cpy);
+	~ScavTrap();
+
 	void	attack(const std::string& target);
-    void    guardGate(void);
+	void	guardGate(void);
 };
 
 #endif
+
+/*MACRO MESSAGES*/
+
+//ATTACK
+
+# define S_ATCK_INIT_MSG \
+"You must initialize this ScavTrap before attacking"\
+	<< std::endl
+# define S_ATCK_TRGT_MSG \
+"You must specify a valid target" << std::endl
+# define S_ATCK_DEAD_MSG \
+"ScavTrap " << UGRN + this->_name + reset << \
+	" can't attack cause it is dead." << std::endl
+# define S_ATCK_NO_NRG_MSG \
+"ScavTrap " << UGRN + this->_name + reset << \
+	" can't attack cause it has no energy left.." << std::endl
+
+//GUARD
+
+# define S_GRD_INIT_MSG \
+"You must initialize this ScavTrap before guarding"\
+	<< std::endl
+# define S_GRD_DEAD_MSG \
+"ScavTrap " << UGRN + this->_name + reset << \
+	" can't guard the gate cause it is dead." << std::endl
+# define S_GRD_NO_NRG_MSG \
+"ScavTrap " << UGRN + this->_name + reset << \
+	" can't guard the gate cause it has no energy left.." << std::endl
